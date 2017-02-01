c89fa6c4c68220f1e01bc02ddbf71dd829509756
Alejandro <Alejandro@AlejandroNV>
2017-02-01 10:50:00 -0600

    Dao 1.0

---
+++ ExamenParcial2/src/main/java/ac/cr/una/backend/dao/BookTypeDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.cr.una.backend.dao;
+
+import ac.cr.una.backend.model.BookType;
+
+/**
+ *
+ * @author Alejandro
+ */
+public interface BookTypeDAO {
+    public boolean deleteAll();
+    public BookType save(BookType bookType);
+    public BookType findByName(String name);
+}
