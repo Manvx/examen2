5806a1b1f75c90c69bcdcab715b19adf11ea0d08
Alejandro <Alejandro@AlejandroNV>
2017-02-01 19:47:53 -0600

    Webservice 3.0

--- ExamenParcial2/src/main/java/ac/cr/una/backend/webservice/AuthorWebService.java
+++ ExamenParcial2/src/main/java/ac/cr/una/backend/webservice/AuthorWebService.java
@@ -6,15 +6,30 @@
 package ac.cr.una.backend.webservice;
 
 import ac.cr.una.backend.dao.AuthorContactDAO;
+import ac.cr.una.backend.dao.AuthorContactDAOImpl;
 import ac.cr.una.backend.dao.AuthorDAO;
+import ac.cr.una.backend.dao.AuthorDAOImpl;
+import ac.cr.una.backend.model.Author;
+import ac.cr.una.backend.model.AuthorContact;
 import ac.cr.una.backend.service.AuthorService;
+import ac.cr.una.backend.service.AuthorServiceImpl;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author Alejandro
  */
+@Path("authors")
 public class AuthorWebService {
     private AuthorDAO authorDAO;
     private AuthorContactDAO authorContactDAO;
@@ -26,5 +41,58 @@ public class AuthorWebService {
     public AuthorWebService() {
     }
     
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<AuthorContact> getAllAuthors() {
+        List<AuthorContact> authorContactList = null;
+        authorContactDAO = new AuthorContactDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        authorContactList = authorService.findAll();
+
+        return authorContactList;
+    }
     
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Author getAuthorByName(@PathParam("name") String name){
+        Author author = null;
+        authorDAO = new AuthorDAOImpl();
+        authorService = new AuthorServiceImpl(authorDAO);
+
+        author = authorService.findByName(name);
+
+        return author;
+    }
+    
+    @DELETE
+    @Path("/")
+    public boolean deleteAll(){
+        boolean delete;
+        authorDAO = new AuthorDAOImpl();
+        authorService = new AuthorServiceImpl(authorDAO);
+
+        delete = authorService.deleteAll();
+
+        return delete;
+    }
+    
+    /**
+     *
+     * @param authorContact
+     */
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public  AuthorContact createAuthor(AuthorContact authorContact){
+        authorContactDAO = new AuthorContactDAOImpl();
+        authorService = new AuthorServiceImpl(authorContactDAO);
+
+        authorContact = authorService.save(authorContact);
+
+        return authorContact;
+    }
 }
--- ExamenParcial2/src/main/java/ac/cr/una/backend/webservice/BookTypeWebService.java
+++ ExamenParcial2/src/main/java/ac/cr/una/backend/webservice/BookTypeWebService.java
@@ -6,14 +6,27 @@
 package ac.cr.una.backend.webservice;
 
 import ac.cr.una.backend.dao.BookTypeDAO;
+import ac.cr.una.backend.dao.BookTypeDAOImpl;
+import ac.cr.una.backend.model.AuthorContact;
+import ac.cr.una.backend.model.BookType;
 import ac.cr.una.backend.service.BookTypeService;
+import ac.cr.una.backend.service.BookTypeServiceImpl;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author Alejandro
  */
+@Path("bookTypes")
 public class BookTypeWebService {
     private BookTypeDAO bookTypeDAO;
     private BookTypeService bookTypeService;
@@ -23,5 +36,43 @@ public class BookTypeWebService {
 
     public BookTypeWebService() {
     }
+    
+     @GET
+    @Path("/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public BookType getByName(@PathParam("name") String name){
+        BookType bookType = null;
+        bookTypeDAO = new BookTypeDAOImpl();
+        bookTypeService = new BookTypeServiceImpl(bookTypeDAO);
+
+        bookType = bookTypeService.findByName(name);
+
+        return bookType;
+    }
+    @DELETE
+    @Path("/")
+    public boolean deleteAll(){
+        boolean delete;
+        bookTypeDAO = new BookTypeDAOImpl();
+        bookTypeService = new BookTypeServiceImpl(bookTypeDAO);
+
+        delete = bookTypeService.deleteAll();
+
+        return delete;
+    }
+    
+     @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public  BookType createBook(BookType bookType){
+        bookTypeDAO = new BookTypeDAOImpl();
+        bookTypeService = new BookTypeServiceImpl(bookTypeDAO);
+
+        bookType = bookTypeService.save(bookType);
+
+        return bookType;
+    }
+    
     
 }
